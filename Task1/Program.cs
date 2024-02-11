8369d03090e63c890cf248ef3cf844279dbe8172
Sure <schurovdaniil@gmail.com>
2024-02-11 18:43:25 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿string Nums(int M, int N)
+{
+    if(M == N) return Convert.ToString(M);
+    return M + " " + Nums(M + 1, N);
+}
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(Nums(M, N));
